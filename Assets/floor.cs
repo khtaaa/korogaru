f6ee96fa58f9d45e6475b04f4d50f0bc1a9c8bf8
khtaaa <kahata1128@gmail.com>
2017-05-22 16:11:56 +0900

    test

---
+++ Assets/floor.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class floor : MonoBehaviour {
+	public Material red;
+	public Material def;
+	public float time;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		time += Time.deltaTime;
+		if (time > 6) {
+			time = 0;
+		}
+		if (time == 0) {
+			this.GetComponent<Renderer> ().material = def;
+		}
+		if (time >= 3) {
+			if (transform.position.x == manager.x || transform.position.z == manager.z) {
+				this.GetComponent<Renderer> ().material = red;
+			}
+		}
+	}
+}
---
+++ Assets/manager.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class manager : MonoBehaviour {
+	public int xx=5;
+	public int zz=-10;
+	public static int x;
+	public static int z;
+	public float time;
+	public GameObject sphere;
+	// Use this for initialization
+	void Start () {
+		x = 5;
+		z = -10;
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		time += Time.deltaTime;
+		if (time >= 6) {
+			x = (int)Random.Range (-2, 2) * 5;
+			z = (int)Random.Range (-2, 2) * 5;
+			time = 0;
+		}
+		if (time >= 3) {
+			Instantiate (sphere, new Vector3 (10, 3, z), Quaternion.identity);
+		}
+
+		xx = x;
+		zz = z;
+		
+	}
+}
